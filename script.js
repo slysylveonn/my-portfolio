b0f74a7336496af96b7a92e3920e33a2d8aa5cb1
Sophie Gelly <sophgelly@gmail.com>
2024-11-25 15:40:09 -0500

    implemented dark mode function and minor styling changes

--- script.js
+++ script.js
@@ -60,3 +60,18 @@ submit.addEventListener('click', function (event) {
         
 
 });
+
+
+
+//dark mode
+
+const darkMode = document.getElementById('drk-toggle');
+
+darkMode.addEventListener('click', function () {
+    document.body.classList.toggle('dark-theme');
+    if (document.body.classList.contains('dark-theme')) {
+        darkMode.textContent = 'Light mode';
+    } else {
+        darkMode.textContent = 'Dark mode';
+    }
+});
